cfea25fea148ea054816b4e87211b9c3f02869fa
meekmillbaba <meekmillbaba@gmail.com>
2023-09-21 03:55:02 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,19 +12,19 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	while (dest[i] !=*\0*)
+	while (dest[i] != *\0)
 	{
 		i++;
 	}
 	j = 0
-	while (src[j] != *\0*)
+	while (src[j] != *\0)
 	{
 		dest[1] = src[j];
 		i++;
 		j++;
 	}
 
-	dest[i] = *\0*;
+	dest[i] = *\0;
 	return (dest);
 }
 
